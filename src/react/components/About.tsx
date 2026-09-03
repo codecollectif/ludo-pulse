664587575a86f1340852c625dea84f3037a7daeb
khaliev <dev.adlan@icloud.com>
2026-09-03 11:45:13 +0200

    connect front to back

--- src/react/components/About.tsx
+++ src/react/components/About.tsx
@@ -1,8 +1,15 @@
+import { use } from "react";
+
+import { getOrFetch } from "../helpers/cache";
+
 function About() {
+  const { message } = use(getOrFetch<{ message: string }>("/api/hello"));
+
   return (
     <>
       <h1>À propos</h1>
       <p>StartER rocks !</p>
+      <p>Message de l'API : "{message}"</p>
     </>
   );
 }
